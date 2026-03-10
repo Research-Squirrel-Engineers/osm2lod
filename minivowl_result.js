0be9ad5f9fc7f4e84995edeb91dc40c435d53c69
florianthiery <mail@fthiery.de>
2026-03-10 22:33:15 +0000

    Deploying to gh-pages from @ Research-Squirrel-Engineers/osm2lod@aab79310bff67be25bc862cc0f4a70fcd33dcfab 🚀

--- minivowl_result.js
+++ minivowl_result.js
@@ -58,167 +58,6 @@ var minivowlresult={
  ],
  "links": [
   {
-   "source": 6,
-   "target": 1,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 6,
-   "target": 0,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 6,
-   "target": 2,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 6,
-   "target": 3,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 9,
-   "target": 3,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 9,
-   "target": 5,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 9,
-   "target": 2,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 9,
-   "target": 4,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 9,
-   "target": 1,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 9,
-   "target": 0,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 8,
-   "target": 3,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 8,
-   "target": 5,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 8,
-   "target": 2,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 8,
-   "target": 4,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 8,
-   "target": 1,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 8,
-   "target": 0,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 7,
-   "target": 3,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 7,
-   "target": 5,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 7,
-   "target": 2,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 7,
-   "target": 4,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 7,
-   "target": 1,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 7,
-   "target": 0,
-   "valueTo": "dataset",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/dcat#dataset"
-  },
-  {
-   "source": 1,
-   "target": 4,
-   "valueTo": "hasGeometry",
-   "propertyTo": "class",
-   "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
-  },
-  {
    "source": 0,
    "target": 4,
    "valueTo": "hasGeometry",
@@ -241,17 +80,164 @@ var minivowlresult={
   },
   {
    "source": 1,
-   "target": 3,
-   "valueTo": "isPartOf",
+   "target": 4,
+   "valueTo": "hasGeometry",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/isPartOf"
+   "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
   },
   {
-   "source": 1,
-   "target": 5,
-   "valueTo": "isPartOf",
+   "source": 6,
+   "target": 0,
+   "valueTo": "member",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/isPartOf"
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 6,
+   "target": 2,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 6,
+   "target": 3,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 6,
+   "target": 1,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 7,
+   "target": 5,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 7,
+   "target": 2,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 7,
+   "target": 4,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 7,
+   "target": 0,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 7,
+   "target": 3,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 7,
+   "target": 1,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 8,
+   "target": 5,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 8,
+   "target": 2,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 8,
+   "target": 4,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 8,
+   "target": 0,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 8,
+   "target": 3,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 8,
+   "target": 1,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 9,
+   "target": 5,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 9,
+   "target": 2,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 9,
+   "target": 4,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 9,
+   "target": 0,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 9,
+   "target": 3,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
+  },
+  {
+   "source": 9,
+   "target": 1,
+   "valueTo": "dataset",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/dcat#dataset"
   },
   {
    "source": 0,
@@ -290,6 +276,20 @@ var minivowlresult={
   },
   {
    "source": 3,
+   "target": 5,
+   "valueTo": "isPartOf",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/isPartOf"
+  },
+  {
+   "source": 1,
+   "target": 3,
+   "valueTo": "isPartOf",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/isPartOf"
+  },
+  {
+   "source": 1,
    "target": 5,
    "valueTo": "isPartOf",
    "propertyTo": "class",
